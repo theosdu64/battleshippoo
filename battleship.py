85db873b68b3a0aa71af564ec604c531ade518ab
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-21 10:40:01 +0200

    place ship in grid and create ship done

--- battleship.py
+++ battleship.py
@@ -18,14 +18,26 @@ class Ship:
 class Game:
     def __init__(self):
         self.grid = Grid()
+        self.ships = []
 
     def print_grid(self):
         self.grid.print_grid()
 
+    def add_ship(self, ship):
+        self.ships.append(ship)
 
 def main():
     game = Game()
     game.print_grid()
+    aircraft = Ship([(1, 'C'), (2, 'C'), (3, 'C')], 'aircraft')
+    cruiser = Ship([(1, 'A'), (2, 'A')], 'cruiser')
+    destroyer = Ship([(9, 'E'), (9, 'F')], 'destroyer')
+    torpedo = Ship([(1, 'J'), (2, 'J')], 'torpedo')
+
+    war_boat = [aircraft, cruiser, destroyer, torpedo]
+
+    for boats in war_boat:
+        game.add_ship(boats)
 
 if __name__ ==  '__main__':
     main()
