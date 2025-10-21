bfc878c6cccdd7a6885e7fa16b92711d0595c07b
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-21 10:30:54 +0200

    create grid class init grid and main func

---
+++ battleship.py
@@ -0,0 +1,18 @@
+import pandas as pd  # type: ignore
+
+class Grid:
+    def __init__(self):
+        self.grid = pd.DataFrame(
+            data=[['~' for _ in range(10)] for _ in range(10)],
+            columns=['A','B','C','D','E','F','G','H','I','J'],
+            index=list(range(1, 11))
+        )
+    def print_grid(self):
+        print(self.grid)
+
+def main():
+    grid = Grid()
+    grid.print_grid()
+
+if __name__ ==  '__main__':
+    main()
